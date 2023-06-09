e35961931589c6efeccc6afaba804e7cd2eabf58
Norik-Chichyan <norikchichyan@gmail.com>
2023-06-09 20:29:15 +0400

    new config added

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.1.0"
-    random = "~> 0.9.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
     }
   }
 }
